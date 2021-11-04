e82f1fcff3506db9bc5d3c5a21b0c7a6153d13da
PANDITHAN <91892960+PANDITHAN@users.noreply.github.com>
2021-11-04 15:28:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,18 +19,17 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url='http://t.me/PANDITHAN_ROBOT?startgroup=true')
             ],[
-            InlineKeyboardButton('©️channel', url='https://t.me/tg_bots_updates'),
-            InlineKeyboardButton('♻️Group', url='https://t.me/EDIT_REPO')
+            InlineKeyboardButton('⭐CHANNEL⭐', url='https://t.me/M_STER_TECH'),
+            InlineKeyboardButton('💫GROUP💫', url='https://t.me/M_STER_TECH_GROUP')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
             ],[
-            InlineKeyboardButton('🇩 🇪 🇻 ', url='https://t.me/pro_editor_tg'),
-            InlineKeyboardButton('🤠Share me', url='https://t.me/share/url?url=https://t.me/Dqautofl_bot')
+            InlineKeyboardButton('🧞‍♂️𝐃𝐄𝐕🧞‍♂️', url='https://t.me/PANDITHAN_SIR')
             ],[
-            InlineKeyboardButton('ANNA UPDATES😉', url='https://t.me/team_annaben')
+            InlineKeyboardButton('⚡Share me⚡', url='https://t.me/share/url?url=https://t.me/PANDITHAN_ROBOT')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -67,18 +66,17 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url='http://t.me/PANDITHAN_ROBOT?startgroup=true')
             ],[
-            InlineKeyboardButton('©️ Channel', url='https://t.me/tg_bots_updates'),
-            InlineKeyboardButton('♻️ Group', url='https://t.me/EDIT_REPO')
+            InlineKeyboardButton('⭐CHANNEL⭐', url='https://t.me/M_STER_TECH'),
+            InlineKeyboardButton('💫GROUP💫', url='https://t.me/M_STER_TECH_GROUP')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
             ],[
-            InlineKeyboardButton('ᦔꫀꪜ', url='https://t.me/pro_editor_tg'), 
-            InlineKeyboardButton('🤠Share me', url='https://t.me/share/url?url=https://t.me/Dqautofl_bot')
+            InlineKeyboardButton('🧞‍♂️𝐃𝐄𝐕🧞‍♂️', url='https://t.me/PANDITHAN_SIR')
             ],[
-            InlineKeyboardButton('ANNA UPDATES😉', url='https://t.me/team_annaben')
+            InlineKeyboardButton('⚡Share me⚡', url='https://t.me/share/url?url=https://t.me/PANDITHAN_ROBOT')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
